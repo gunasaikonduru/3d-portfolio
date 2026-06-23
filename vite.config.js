c5a830f7e7622db45dcf9e58e7e0206ccd74846d
gunasaikonduru <gunasaikonduru262@gmail.com>
2026-06-23 18:08:30 +0530

    Update vite.config.js base and repo settings

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
-  base: mode === "production" ? "/3d-portfolio-website/" : "/",
+  base: mode === "production" ? "/3d-portfolio/" : "/",
 }));
