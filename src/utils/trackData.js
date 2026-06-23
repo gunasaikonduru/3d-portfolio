14aff126b0d3b2a7612b4218cb93b25d6d5ee6b9
gunasaikonduru <gunasaikonduru262@gmail.com>
2026-06-23 17:30:52 +0530

    Upload src/utils/trackData.js via automation

---
+++ src/utils/trackData.js
@@ -0,0 +1,52 @@
+export const tracks = [
+  {
+    title: "Hamon",
+    src: "music/hamon.wav",
+    image: "images/music/hamon.jpg",
+  },
+  {
+    title: "Denno",
+    src: "music/denno.wav",
+    image: "images/music/denno.jpg",
+  },
+  {
+    title: "Hikari",
+    src: "music/hikari.wav",
+    image: "images/music/hikari.webp",
+  },
+  {
+    title: "Kuttenete",
+    src: "music/kuttenete.wav",
+    image: "images/music/kuttenete.jpg",
+  },
+  {
+    title: "Night",
+    src: "music/night.wav",
+    image: "images/music/night.webp",
+  },
+  {
+    title: "Rojiura",
+    src: "music/rojiura.wav",
+    image: "images/music/rojiura.webp",
+  },
+  {
+    title: "Sabaku",
+    src: "music/sabaku.wav",
+    image: "images/music/sabaku.webp",
+  },
+  {
+    title: "Shinkirou",
+    src: "music/shinkirou.wav",
+    image: "images/music/shinkirou.webp",
+  },
+  {
+    title: "Shizuka No Umi",
+    src: "music/shizuka_no_umi.wav",
+    image: "images/music/shizuka_no_umi.webp",
+  },
+  {
+    title: "Yorugao",
+    src: "music/yorugao.wav",
+    image: "images/music/yorugao.webp",
+  },
+];
